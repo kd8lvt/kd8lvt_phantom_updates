d38e611ff0a923e3c84d809156bbe87f1fe2176b
kd8lvt <redunderpant@gmail.com>
2019-01-23 13:21:45 -0500

    initial commit

---
+++ launch.bat
@@ -0,0 +1,32 @@
+@echo off
+
+REM  
+REM Copyright (C) 2016-2018 phantombot.tv
+REM  
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation, either version 3 of the License, or
+REM (at your option) any later version.
+REM 
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM  
+REM You should have received a copy of the GNU General Public License
+REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+REM
+
+setlocal enableextensions enabledelayedexpansion
+set first=1
+for /f "delims=" %%a in ("%comspec%") do set "compath=%%~DPa"
+PATH %PATH%;%compath%;%JAVA_HOME%\bin\
+WHERE java >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+    echo You must have Java installed, please install it from: https://java.com/download
+    pause
+    exit
+)
+java -Dinteractive -Xms1m -Dfile.encoding=UTF-8 -jar PhantomBot.jar %1
+endlocal
+pause
---
+++ launch.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Copyright (C) 2016-2018 phantombot.tv
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# PhantomBot Launcher - Linux and macOS
+#
+# Please run the following to launch the bot, the chmod is required only once.
+# % chmod +x launch.sh
+# % ./launch.sh
+#
+
+unset DISPLAY
+
+if [[ $(uname)=="Darwin" ]]; then
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    cd "$DIR"
+else
+    cd $(dirname $(readlink -f $0))
+fi
+
+if type -p java 1>/dev/null 2>/dev/null; then
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "You don't have Java installed! Download it from https://www.java.com/en/download/"
+fi
+
+java -Dinteractive -Xms1m -Dfile.encoding=UTF-8 -jar PhantomBot.jar ${1}
